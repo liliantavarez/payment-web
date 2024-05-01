f45c1c48f7f0d1e013d18749b18725d45306ee84
Lilian Carvalho <liliancarvalhotavares@gmail.com>
2024-05-01 10:54:02 -0300

    refactor: alterando diretório do arquivo

--- src/modules/payment/routes/router.ts
+++ src/modules/payment/routes/router.ts
@@ -1,7 +1,10 @@
 import { Router } from "express";
+import { paymentRouter } from "./payments/paymentsRoutes.js";
 
 const router = Router();
 
 router.get("/", (_, res) => res.json({ message: "Bem vindo a API payment-web" }));
+router.use("/payments", paymentRouter);
 router.get("*", (_, res) => res.json({ message: "Rota não encontrada" }));
+
 export { router };
