bf928c89689b10abba53b6620c2f835ad3c3cbfe
Lilian Carvalho <liliancarvalhotavares@gmail.com>
2024-05-02 18:51:54 -0300

    feat: definições de cores

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -8,10 +8,24 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        b600: '#3182CE',
+        c300: '#E2E8F0',
+        c600: '#718096',
+        v500: '#38B2AC', 
+        c700: '#4A5568',
+        b800: '#2D3748',
+        light: '#f8fafc',  
+        DEFAULT: '#000000', 
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+
+      boxShadow: {
+        'custom': '0px 4px 4px 0px #00000040',
       },
     },
   },
